c608c81319c5b81a398284776c7c481c31d4b565
coreysn <95398533+coreysn@users.noreply.github.com>
2023-04-25 17:00:22 -0600

    Add welcome page

--- Dockerfile
+++ Dockerfile
@@ -1,27 +1,23 @@
-# Official Dart image: https://hub.docker.com/_/dart
-# Specify the Dart SDK base image version using dart:<version> (ex: dart:2.12)
-FROM dart:stable AS build
+FROM dart:2.19.6-sdk AS build
 
-# Resolve app dependencies.
-WORKDIR /app
+WORKDIR /usr/src/app
 COPY pubspec.* ./
 RUN dart pub get
 
-# Copy app source code and AOT compile it.
-COPY . .
-# Ensure packages are still up-to-date if anything has changed
+COPY bin bin/
+COPY public public/
+
 RUN dart pub get --offline
 RUN dart compile exe bin/server.dart -o bin/server
 
-# Build minimal serving image from AOT-compiled `/server` and required system
-# libraries and configuration files stored in `/runtime/` from the build stage.
 FROM scratch
+
 COPY --from=build /runtime/ /
-COPY --from=build /app/bin/server /app/bin/
 
-# Include files in the /public directory to enable static asset handling
-COPY --from=build /app/public/ /public
+WORKDIR /usr/src/app
 
-# Start server.
+COPY --from=build /usr/src/app/bin/server bin/
+COPY --from=build /usr/src/app/public/ public/
+
 EXPOSE 8080
-CMD ["/app/bin/server"]
+CMD ["./bin/server"]
--- bin/server.dart
+++ bin/server.dart
@@ -1,3 +1,5 @@
+// Copyright (c) 2021, the Dart project authors.
+
 import 'dart:convert';
 import 'dart:io';
 
@@ -7,36 +9,20 @@ import 'package:shelf_router/shelf_router.dart' as shelf_router;
 import 'package:shelf_static/shelf_static.dart' as shelf_static;
 
 Future<void> main() async {
-  // If the "PORT" environment variable is set, listen to it. Otherwise, 8080.
-  // https://cloud.google.com/run/docs/reference/container-contract#port
   final port = int.parse(Platform.environment['PORT'] ?? '8080');
 
-  // See https://pub.dev/documentation/shelf/latest/shelf/Cascade-class.html
   final cascade = Cascade()
-      // First, serve files from the 'public' directory
-      .add(_staticHandler)
-      // If a corresponding file is not found, send requests to a `Router`
-      .add(_router);
+    .add(_staticHandler);
 
-  // See https://pub.dev/documentation/shelf/latest/shelf_io/serve.html
   final server = await shelf_io.serve(
-    // See https://pub.dev/documentation/shelf/latest/shelf/logRequests.html
     logRequests()
-        // See https://pub.dev/documentation/shelf/latest/shelf/MiddlewareExtensions/addHandler.html
-        .addHandler(cascade.handler),
-    InternetAddress.anyIPv4, // Allows external connections
+      .addHandler(cascade.handler),
+    InternetAddress.anyIPv4,
     port,
   );
 
-  print('Serving at http://${server.address.host}:${server.port}');
+  print('App listening on port ${server.port}');
 }
 
-// Serve files from the file system.
 final _staticHandler =
-    shelf_static.createStaticHandler('public', defaultDocument: 'index.html');
-
-// Router instance to handler requests.
-final _router = shelf_router.Router()
-  ..get('/helloworld', _helloWorldHandler);
-
-Response _helloWorldHandler(Request request) => Response.ok('Hello World!');
+  shelf_static.createStaticHandler('public', defaultDocument: 'index.html');
